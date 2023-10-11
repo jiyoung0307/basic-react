272033ed5fb37c2ff41acc629a2c9a8a3e81b963
zy_zero <jiyoungjang0307@gmail.com>
2023-10-12 00:45:43 +0900

    feat: 초기파일 간략한 설명 v1.0

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,3 +1,4 @@
+// 페이지를 켰을 때 제일 먼저 실행되는 파일
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -6,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  // 나중에 HTML로 rendering되는 부분
   <React.StrictMode>
     <App />
   </React.StrictMode>
@@ -14,4 +16,5 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// 리액트 웹 퍼포먼스 성능 측정 목적
 reportWebVitals();
